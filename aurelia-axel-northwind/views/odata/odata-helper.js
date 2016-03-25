775ca2925797b6fa12c18523ce628a353ca82143
Mike Graham <mgraham@technicalmedia.com>
2016-03-25 09:40:10 -0600

    rough in odata helper (typescript class) - provides fluent api for building odata urls

---
+++ aurelia-axel-northwind/views/odata/odata-helper.js
@@ -0,0 +1,89 @@
+define(["require", "exports"], function (require, exports) {
+    var OdataHelper = (function () {
+        function OdataHelper() {
+            var _this = this;
+            this.inlineCountProp = false;
+            this.buildQuery = function () {
+                // error if no url
+                if (_this.urlProp === null || _this.urlProp.length < 1) {
+                    return { valid: false, message: 'Error in OdataHelper: missing required url' };
+                }
+                // error if no from clause
+                if (!_this.fromProp) {
+                    return { valid: false, message: ('Error in OdataHelper: missing required fromm') };
+                }
+                // build query
+                var query = _this.urlProp + "/" + _this.fromProp + "?"; // $format=json";
+                // add filter if present
+                if (_this.filterProp) {
+                    query += "&$filter=" + _this.filterProp;
+                }
+                // add select if present
+                if (_this.selectProp) {
+                    query += "&$select=" + _this.selectProp;
+                }
+                // add orderBy if present
+                if (_this.orderByProp) {
+                    query += "&$orderby=" + _this.orderByProp + (_this.descProp ? ' desc' : '');
+                }
+                // add skip if present
+                if (_this.skipProp) {
+                    query += "&$skip=" + _this.skipProp;
+                }
+                // add take if present
+                if (_this.takeProp) {
+                    query += "&$top=" + _this.takeProp;
+                }
+                // add inlineCount if present
+                if (_this.inlineCountProp) {
+                    query += "&$inlinecount=allpages";
+                }
+                // add expand if present
+                if (_this.expandProp) {
+                    query += "&$expand=" + _this.expandProp;
+                }
+                return { valid: true, query: query };
+            };
+            this.url = function (url) {
+                _this.urlProp = url;
+                return _this;
+            };
+            this.fromm = function (fromm) {
+                _this.fromProp = fromm;
+                return _this;
+            };
+            this.filter = function (filter) {
+                _this.filterProp = filter;
+                return _this;
+            };
+            this.select = function (select) {
+                _this.selectProp = select;
+                return _this;
+            };
+            this.orderBy = function (orderBy, desc) {
+                _this.orderByProp = orderBy;
+                _this.descProp = desc;
+                return _this;
+            };
+            this.skip = function (count) {
+                _this.skipProp = count;
+                return _this;
+            };
+            this.take = function (count) {
+                _this.takeProp = count;
+                return _this;
+            };
+            this.inlineCount = function () {
+                _this.inlineCountProp = true;
+                return _this;
+            };
+            this.expand = function (expand) {
+                _this.expandProp = expand;
+                return _this;
+            };
+        }
+        return OdataHelper;
+    })();
+    exports.OdataHelper = OdataHelper;
+});
+//# sourceMappingURL=odata-helper.js.map
---
+++ aurelia-axel-northwind/views/odata/odata-helper.ts
@@ -0,0 +1,120 @@
+﻿interface BuildQueryResult {
+    valid: boolean,
+    message?: string,
+    query?: string
+}
+
+export class OdataHelper {
+    urlProp: string;
+    fromProp: string;
+    filterProp: string;
+    selectProp: string;
+    orderByProp: string;
+    descProp: boolean;
+    skipProp: number;
+    takeProp: number;
+    inlineCountProp: boolean = false;
+    expandProp: string;
+
+    constructor() {
+    }
+
+    buildQuery = (): BuildQueryResult => {
+        
+        // error if no url
+        if (this.urlProp === null || this.urlProp.length < 1) {
+            return { valid: false, message: 'Error in OdataHelper: missing required url' };
+        }
+
+        // error if no from clause
+        if (!this.fromProp) {
+            return { valid: false, message: ('Error in OdataHelper: missing required fromm') };
+        }
+
+        // build query
+        var query = this.urlProp + "/" + this.fromProp + "?"; // $format=json";
+
+        // add filter if present
+        if (this.filterProp) {
+            query += "&$filter=" + this.filterProp;
+        }
+
+        // add select if present
+        if (this.selectProp) {
+            query += "&$select=" + this.selectProp;
+        }
+
+        // add orderBy if present
+        if (this.orderByProp) {
+            query += "&$orderby=" + this.orderByProp + (this.descProp ? ' desc' : '');
+        }
+
+        // add skip if present
+        if (this.skipProp) {
+            query += "&$skip=" + this.skipProp;
+        }
+
+        // add take if present
+        if (this.takeProp) {
+            query += "&$top=" + this.takeProp;
+        }
+
+        // add inlineCount if present
+        if (this.inlineCountProp) {
+            query += "&$inlinecount=allpages";
+        }
+
+        // add expand if present
+        if (this.expandProp) {
+            query += "&$expand=" + this.expandProp;
+        }
+
+        return { valid: true, query: query };
+    }
+
+    url = (url: string): OdataHelper => {
+        this.urlProp = url;
+        return this;
+    }
+
+    fromm = (fromm: string): OdataHelper => {
+        this.fromProp = fromm;
+        return this;
+    }
+
+    filter = (filter: string): OdataHelper => {
+        this.filterProp = filter;
+        return this;
+    }
+
+    select = (select: string): OdataHelper => {
+        this.selectProp = select;
+        return this;
+    }
+
+    orderBy = (orderBy: string, desc?: boolean): OdataHelper => {
+        this.orderByProp = orderBy;
+        this.descProp = desc;
+        return this;
+    }
+
+    skip = (count: number): OdataHelper => {
+        this.skipProp = count;
+        return this;
+    }
+
+    take = (count: number): OdataHelper => {
+        this.takeProp = count;
+        return this;
+    }
+
+    inlineCount = (): OdataHelper => {
+        this.inlineCountProp = true;
+        return this;
+    }
+
+    expand = (expand: string): OdataHelper => {
+        this.expandProp = expand;
+        return this;
+    }
+}
